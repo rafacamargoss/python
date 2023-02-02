bdd90f4764124f7d0b4d7d18a72d9afe8c7c03fd
rafaelcamargoss <rafael.camargos68@gmail.com>
2023-02-01 20:41:07 -0400

    Aprendendo sobre funcao main e finalizacao do projeto

--- adivinhacao.py
+++ adivinhacao.py
@@ -56,3 +56,8 @@ def jogar():
             pontos = pontos - pontos_perdidos
 
     print("Fim do jogo")
+
+##This allows the code to determine
+##if it's being executed as the main program or being imported as a module into another program.
+if(__name__ == "__main__"):
+    jogar()
--- forca.py
+++ forca.py
@@ -6,3 +6,8 @@ def jogar():
 
 
     print("Fim do jogo")
+
+##This allows the code to determine
+##if it's being executed as the main program or being imported as a module into another program.
+if(__name__ == "__main__"):
+    jogar()
