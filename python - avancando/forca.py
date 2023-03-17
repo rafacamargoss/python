45ebafea3aa81348c9652dc4909e8681850f24f6
Rafael Camargos <rafael.camargos68@gmail.com>
2023-03-16 20:49:48 -0400

    avancando

--- python - avancando/forca.py
+++ python - avancando/forca.py
@@ -5,22 +5,26 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     #enquanto não enforcou e não acertou
     while(not enforcou and not acertou):
         chute = input("Qual letra?")
         chute = chute.strip()
 
-        index = 0
+        index = 0 #posicao
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_acertadas[index] = letra
+                #print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
 
-        print("Jogando..")
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
