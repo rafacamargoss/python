8e5900804c5c6960ec5cd0e3008221d964607a98
rafacamargoss <rafael.camargos68@gmail.com>
2023-03-17 22:07:59 -0300

    implementando list comprehension

--- python - avancando/forca.py
+++ python - avancando/forca.py
@@ -4,28 +4,38 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "maçã".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     #enquanto não enforcou e não acertou
     while(not enforcou and not acertou):
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0 #posicao
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-                #print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0 #posicao
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                    #print("Encontrei a letra {} na posição {}".format(letra, index))
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if (acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
 ##This allows the code to determine
